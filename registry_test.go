b107ee129bda5f857031f3f46b1034f038adf91f
Thunder33345 <Thunder33345@users.noreply.github.com>
2022-08-01 01:24:39 +0800

    improved lookup test

--- registry_test.go
+++ registry_test.go
@@ -3,6 +3,7 @@ package asndb
 import (
 	"net/netip"
 	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -83,82 +84,201 @@ func TestNewRegistry(t *testing.T) {
 	}
 }
 
-func TestRegistry_Lookup(t *testing.T) {
-	r := NewRegistry([]AS{
-		{
-			StartIP:  netip.MustParseAddr("1.0.0.0"),
-			EndIP:    netip.MustParseAddr("1.255.255.255"),
-			ASNumber: 1,
-		}, {
-			StartIP:  netip.MustParseAddr("2.0.0.0"),
-			EndIP:    netip.MustParseAddr("2.255.255.255"),
-			ASNumber: 2,
-		}, {
-			StartIP:  netip.MustParseAddr("3.0.0.0"),
-			EndIP:    netip.MustParseAddr("3.1.255.255"),
-			ASNumber: 3,
-		}, {
-			StartIP:  netip.MustParseAddr("3.2.0.0"),
-			EndIP:    netip.MustParseAddr("3.2.255.255"),
-			ASNumber: 4,
-		},
-	})
-
-	tests := []struct {
+func TestRegistry_Lookup2(t *testing.T) {
+	type subtest struct {
 		name      string
 		ip        netip.Addr
-		want      int
-		wantFound bool
+		wantEmpty bool
+		wantASN   int
+		wantAS    AS
+	}
+	tests := []struct {
+		name          string
+		r             *Registry
+		assumeCorrect bool
+		lookups       []subtest
 	}{
 		{
-			name:      "not found lower bound",
-			ip:        netip.MustParseAddr("0.1.1.1"),
-			wantFound: false,
-		},
-		{
-			name:      "1",
-			ip:        netip.MustParseAddr("1.1.1.1"),
-			want:      1,
-			wantFound: true,
+			//this is a simple gap less lookup, checks for upper and lower bounds
+			name: "simple lookup",
+			r: NewRegistry([]AS{
+				{
+					StartIP:  netip.MustParseAddr("1.0.0.0"),
+					EndIP:    netip.MustParseAddr("1.255.255.255"),
+					ASNumber: 1,
+				}, {
+					StartIP:  netip.MustParseAddr("2.0.0.0"),
+					EndIP:    netip.MustParseAddr("2.255.255.255"),
+					ASNumber: 2,
+				}, {
+					StartIP:  netip.MustParseAddr("3.0.0.0"),
+					EndIP:    netip.MustParseAddr("3.1.255.255"),
+					ASNumber: 3,
+				}, {
+					StartIP:  netip.MustParseAddr("3.2.0.0"),
+					EndIP:    netip.MustParseAddr("3.2.255.255"),
+					ASNumber: 4,
+				},
+			}),
+			lookups: []subtest{
+				{
+					name:      "not found lower bound",
+					ip:        netip.MustParseAddr("0.1.1.1"),
+					wantEmpty: true,
+				},
+				{
+					name:    "1",
+					ip:      netip.MustParseAddr("1.1.1.1"),
+					wantASN: 1,
+					wantAS: AS{
+						StartIP:  netip.MustParseAddr("1.0.0.0"),
+						EndIP:    netip.MustParseAddr("1.255.255.255"),
+						ASNumber: 1,
+					},
+				}, {
+					name:    "2",
+					ip:      netip.MustParseAddr("2.2.2.2"),
+					wantASN: 2,
+				}, {
+					name:    "3-1",
+					ip:      netip.MustParseAddr("3.0.3.3"),
+					wantASN: 3,
+				}, {
+					name:    "3-2",
+					ip:      netip.MustParseAddr("3.1.3.3"),
+					wantASN: 3,
+				}, {
+					name:    "4",
+					ip:      netip.MustParseAddr("3.2.2.2"),
+					wantASN: 4,
+				}, {
+					name:      "not found higher bound",
+					ip:        netip.MustParseAddr("55.55.55.55"),
+					wantEmpty: true,
+				},
+			},
 		}, {
-			name:      "2",
-			ip:        netip.MustParseAddr("2.2.2.2"),
-			want:      2,
-			wantFound: true,
+			//this is a gap-ed lookup, checks for midpoint
+			name: "gap test",
+			r: NewRegistry([]AS{
+				{
+					StartIP:  netip.MustParseAddr("1.0.0.0"),
+					EndIP:    netip.MustParseAddr("1.255.255.255"),
+					ASNumber: 1,
+				}, {
+					StartIP:  netip.MustParseAddr("3.0.0.0"),
+					EndIP:    netip.MustParseAddr("3.255.255.255"),
+					ASNumber: 2,
+				},
+			}),
+			lookups: []subtest{
+				{
+					name:      "not found lower bound",
+					ip:        netip.MustParseAddr("0.1.1.1"),
+					wantEmpty: true,
+				},
+				{
+					name:    "1",
+					ip:      netip.MustParseAddr("1.1.1.1"),
+					wantASN: 1,
+				}, {
+					name:      "middle gap",
+					ip:        netip.MustParseAddr("2.2.2.2"),
+					wantEmpty: true,
+				}, {
+					name:    "3-1",
+					ip:      netip.MustParseAddr("3.0.3.3"),
+					wantASN: 2,
+				}, {
+					name:    "3-2",
+					ip:      netip.MustParseAddr("3.1.3.3"),
+					wantASN: 2,
+				}, {
+					name:      "not found higher bound",
+					ip:        netip.MustParseAddr("55.55.55.55"),
+					wantEmpty: true,
+				},
+			},
 		}, {
-			name:      "3-1",
-			ip:        netip.MustParseAddr("3.0.3.3"),
-			want:      3,
-			wantFound: true,
-		}, {
-			name:      "3-2",
-			ip:        netip.MustParseAddr("3.1.3.3"),
-			want:      3,
-			wantFound: true,
-		}, {
-			name:      "4",
-			ip:        netip.MustParseAddr("3.2.2.2"),
-			want:      4,
-			wantFound: true,
-		}, {
-			name:      "not found higher bound",
-			ip:        netip.MustParseAddr("55.55.55.55"),
-			wantFound: false,
+			//this check midpoint but checks if assumptions is correct
+			name: "assume valid gap test",
+			r: NewRegistry([]AS{
+				{
+					StartIP:  netip.MustParseAddr("1.0.0.0"),
+					EndIP:    netip.MustParseAddr("1.255.255.255"),
+					ASNumber: 1,
+				}, {
+					StartIP:  netip.MustParseAddr("3.0.0.0"),
+					EndIP:    netip.MustParseAddr("3.255.255.255"),
+					ASNumber: 2,
+				}, {
+					StartIP:  netip.MustParseAddr("5.0.0.0"),
+					EndIP:    netip.MustParseAddr("5.255.255.255"),
+					ASNumber: 3,
+				},
+			}, WithAssumeValid()),
+			assumeCorrect: true,
+			lookups: []subtest{
+				{
+					name:    "1",
+					ip:      netip.MustParseAddr("1.1.1.1"),
+					wantASN: 1,
+				}, {
+					name:    "middle gap assume 1",
+					ip:      netip.MustParseAddr("2.2.2.2"),
+					wantASN: 1,
+				}, {
+					name:    "2",
+					ip:      netip.MustParseAddr("3.0.3.3"),
+					wantASN: 2,
+				}, {
+					name:    "middle gap assume 2",
+					ip:      netip.MustParseAddr("4.2.2.2"),
+					wantASN: 2,
+				}, {
+					name:    "3",
+					ip:      netip.MustParseAddr("5.0.3.3"),
+					wantASN: 3,
+				},
+			},
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := r.Lookup(tt.ip)
-			if got1 != tt.wantFound {
-				t.Errorf("Lookup() found = %v, want %v", got1, tt.wantFound)
-			}
-			if tt.wantFound == false {
-				return
-			}
-			if !reflect.DeepEqual(got.ASNumber, tt.want) {
-				t.Errorf("Lookup() ASN = %v, want %v", got.ASNumber, tt.want)
-			}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			r := tc.r
+			for _, tt := range tc.lookups {
+				t.Run(tt.name, func(t *testing.T) {
+					gotAS, gotFound := r.Lookup(tt.ip)
+					if tt.wantEmpty {
+						if gotFound {
+							t.Errorf("Registry.Lookup() Found = %v, want false", gotFound)
+						}
+						if gotAS != (AS{}) {
+							t.Errorf("Registry.Lookup() AS = %v, want empty", gotAS)
+						}
+					}
 
+					if (!tt.wantEmpty) != gotFound {
+						t.Errorf("Registry.Lookup() Found = %v, want %t", gotFound, !tt.wantEmpty)
+					}
+					if gotAS.ASNumber != tt.wantASN {
+						t.Errorf("Registry.Lookup() ASN = %v, want %v", gotAS.ASNumber, tt.wantASN)
+					}
+					if tt.wantAS.StartIP.IsValid() && tt.wantAS.EndIP.IsValid() {
+						if !reflect.DeepEqual(gotAS, tt.wantAS) {
+							t.Errorf("Registry.Lookup() AS = %v, want %v", gotAS, tt.wantAS)
+						}
+					}
+					if tc.assumeCorrect == false && !tt.wantEmpty {
+						if gotAS.Contains(tt.ip) == false {
+							t.Errorf("Registry.Lookup() AS does not actually contain %v", tt.ip)
+						}
+					}
+					if t.Failed() {
+						t.Logf("Registry.Lookup() AS = %v, Found = %v, ASN = %d", gotAS, gotFound, gotAS.ASNumber)
+					}
+				})
+			}
 		})
 	}
 }
@@ -369,4 +489,56 @@ func TestRegistry_Integration(t *testing.T) {
 	})
 
 	_ = r.s[0].String()
+}
+
+func TestAsSort(t *testing.T) {
+	asl := []AS{
+		{
+			ASNumber: 0,
+			StartIP:  netip.MustParseAddr("0.5.0.0"),
+		},
+		{
+			ASNumber: 1,
+			StartIP:  netip.MustParseAddr("1.0.0.0"),
+		}, {
+			ASNumber: 3,
+			StartIP:  netip.MustParseAddr("3.0.0.0"),
+		}, {
+			ASNumber: 2,
+			StartIP:  netip.MustParseAddr("2.0.0.0"),
+		}, {
+			ASNumber: 4,
+			StartIP:  netip.MustParseAddr("4.0.0.0"),
+		},
+	}
+	t.Run("SortIP", func(t *testing.T) {
+		c := asSortIP(clone(asl))
+		sort.Sort(c)
+		for i, as := range c {
+			if as.ASNumber != i {
+				t.Errorf("list[%d].ASNumber = %v, want %v", i, as.ASNumber, i)
+			}
+		}
+		if t.Failed() {
+			t.Logf("asl: %v", c)
+		}
+		if reflect.DeepEqual(asl, c) {
+			t.Errorf("asl and c should not be equal(asl altered?)")
+		}
+	})
+	t.Run("SortASN", func(t *testing.T) {
+		c := asSortASN(clone(asl))
+		sort.Sort(c)
+		for i, as := range c {
+			if as.ASNumber != i {
+				t.Errorf("list[%d].ASNumber = %v, want %v", i, as.ASNumber, i)
+			}
+		}
+		if t.Failed() {
+			t.Logf("asl: %v", c)
+		}
+		if reflect.DeepEqual(asl, c) {
+			t.Errorf("asl and c should not be equal(asl altered?)")
+		}
+	})
 }
